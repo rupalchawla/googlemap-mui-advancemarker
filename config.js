a64d5aab670397beb74c5a36e89ef8611e1567a2
Rupal Chawla <rupal.chawla@gmail.com>
2024-11-16 14:49:28 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Purpose: Define the type of the config object
 
 const config = {
-    API_KEY: "AIzaSyCfiapuc-uSIZadBkswqSACUTc7Q1XKDgo",
-    MAP_ID: "c7b2b7b2b7b2b7b2"
+    API_KEY: "",
+    MAP_ID: ""
 }
 
 export default config;
